cebab5e6f0c24e688600503e6d521f5d0aa7f2e8
sabrinazulkfl <sabrinazulkfl@gmail.com>
2022-02-02 02:04:49 +0000

    Create letter.cpp

---
+++ letter.cpp
@@ -0,0 +1,13 @@
+//Letter to Self
+
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "Dear Self,\n";
+  std::cout << "\n\tPlease dont give up learning this. You can do this!
+    Also, please enjoy yourself :)\n";
+  std::cout << "\nSabrina.";
+
+}
