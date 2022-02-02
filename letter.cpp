50db2440c34c18eab84b17a3242d8cbb608066e5
sabrinazulkfl <sabrinazulkfl@gmail.com>
2022-02-02 04:17:01 +0000

    std::cout

--- letter.cpp
+++ letter.cpp
@@ -3,10 +3,8 @@
 
 int main()
 {
-
   std::cout << "Dear Self,\n";
   std::cout << "\n\tPlease dont give up learning this. You can do this!
     Also, please enjoy yourself :)\n";
   std::cout << "\nSabrina.";
-
 }
--- pattern.cpp
+++ pattern.cpp
@@ -8,5 +8,4 @@ int main()
   std::cout << "    2 3\n";
   std::cout << "  4 5 6\n";
   std::cout << "7 8 9 10\n";
-
 }
